3c411202735c0336f505921358e935c086ab3b87
GitHub Action <action@github.com>
2023-03-25 08:56:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/yunabraska/githubworkflowpligun/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/yunabraska/githubworkflowpligun/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.yunabraska.githubworkflowpligun.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.yunabraska.githubworkflowpligun.MyBundle
+import com.github.yunabraska.githubworkflowpligun.services.MyProjectService
 import javax.swing.JButton
 
 
