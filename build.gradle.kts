11cd43f78d6b192e63f9f993a31e06a05c5a6b8b
Yuna <io@yuna.berlin>
2024-08-15 16:01:48 +0200

    chore: update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,130 +3,118 @@ import org.jetbrains.changelog.markdownToHTML
 
 fun properties(key: String) = providers.gradleProperty(key)
 fun environment(key: String) = providers.environmentVariable(key)
+fun projectProperties(key: String) = project.findProperty(key).toString()
 
 plugins {
     // Java support
     kotlin("jvm") version "2.0.0"
     id("java")
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.intellij.platform") version "2.0.1"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.0.0"
-    // Gradle Qodana Plugin
-//    id("org.jetbrains.qodana") version "2024.1.5"
 }
 
-group = properties("pluginGroup").get()
-version = properties("pluginVersion").get()
+val pluginId = projectProperties("pluginId")
+val pluginName = projectProperties("pluginName")
+val pluginDescription = projectProperties("pluginDescription")
+val pluginGroup = projectProperties("pluginGroup")
+val pluginVersion = projectProperties("pluginVersion")
+val pluginSinceBuild = projectProperties("pluginSinceBuild")
+val pluginUntilBuild = projectProperties("pluginUntilBuild")
+val vendorName = projectProperties("vendorName")
+val vendorEmail = projectProperties("vendorEmail")
+val pluginUrl = projectProperties("pluginUrl")
+val platformVersion = projectProperties("platformVersion")
+val platformPlugins = properties("platformPlugins").map { it.split(',') }
+
+println("pluginId [$pluginId]")
+println("pluginName [$pluginName]")
+println("pluginGroup [$pluginGroup]")
+println("pluginDescription [$pluginDescription]")
+println("pluginVersion [$pluginVersion]")
+println("platformVersion [$platformVersion]")
+println("pluginSinceBuild [$pluginSinceBuild]")
+println("pluginUntilBuild [$pluginUntilBuild]")
+println("vendorName [$vendorName]")
+println("vendorEmail [$vendorEmail]")
+println("pluginUrl [$pluginUrl]")
+println("platformPlugins:\n${platformPlugins.get().joinToString(separator = "\n") { "  - $it" }}")
+
+group = pluginGroup
+version = pluginVersion
 
 // Configure project's dependencies
 repositories {
     mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+        intellijDependencies()
+    }
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellijPlatform {
+    pluginConfiguration {
+        id = pluginId
+        name = pluginName
+        version = pluginVersion
+        changeNotes.set(provider {
+            changelog.renderItem(changelog.getLatest(), Changelog.OutputType.HTML)
+        })
+        description = pluginDescription
+
+        ideaVersion {
+            sinceBuild.set(pluginSinceBuild)
+            untilBuild.set(pluginUntilBuild)
+        }
+
+        vendor {
+            name.set(vendorName)
+            email.set(vendorEmail)
+            url.set(pluginUrl)
+        }
+    }
+
+//    pluginVerification {
+//        failureLevel = listOf(VerifyPluginTask.FailureLevel.COMPATIBILITY_PROBLEMS)
+//
+//        ides {
+//            recommended()
+//        }
+//    }
+
+    publishing {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+
+    signing {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
 }
 
 dependencies {
     testImplementation("org.assertj:assertj-core:3.26.3")
+    intellijPlatform {
+        instrumentationTools()
+        intellijIdeaCommunity(platformVersion)
+        pluginVerifier()
+        bundledPlugins(platformPlugins)
+    }
 }
 
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
-// Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    pluginName.set(properties("pluginName"))
-    version.set(properties("platformVersion"))
-    type.set(properties("platformType"))
-
-    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    plugins.set(properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) })
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
 changelog {
     groups.empty()
-    repositoryUrl.set(properties("pluginRepositoryUrl"))
-}
-
-// ########## DEACTIVATED too much effort to debug changes every new version ##########
-// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
-//qodana {
-//    cachePath.set(provider { file(".qodana").canonicalPath })
-//    reportPath.set(provider { file("build/reports/inspections").canonicalPath })
-//    saveReport.set(true)
-//    showReport.set(environment("QODANA_SHOW_REPORT").map { it.toBoolean() }.getOrElse(false))
-//}
-
-tasks {
-    wrapper {
-        gradleVersion = properties("gradleVersion").get()
-    }
-
-    patchPluginXml {
-        version.set(properties("pluginVersion"))
-        sinceBuild.set(properties("pluginSinceBuild"))
-        untilBuild.set(properties("pluginUntilBuild"))
-
-        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        pluginDescription.set(providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
-            val start = "<!-- Plugin description -->"
-            val end = "<!-- Plugin description end -->"
-
-            with(it.lines()) {
-                if (!containsAll(listOf(start, end))) {
-                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-                }
-                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
-            }
-        })
-
-        val changelog = project.changelog // local variable for configuration cache compatibility
-        // Get the latest available change notes from the changelog file
-        changeNotes.set(properties("pluginVersion").map { pluginVersion ->
-            with(changelog) {
-                renderItem(
-                    (getOrNull(pluginVersion) ?: getUnreleased())
-                        .withHeader(false)
-                        .withEmptySections(false),
-                    Changelog.OutputType.HTML,
-                )
-            }
-        })
-    }
-
-    // Configure UI tests plugin
-    // Read more: https://github.com/JetBrains/intellij-ui-test-robot
-    runIdeForUiTests {
-        systemProperty("robot-server.port", "8082")
-        systemProperty("ide.mac.message.dialogs.as.sheets", "false")
-        systemProperty("jb.privacy.policy.text", "<!--999.999-->")
-        systemProperty("jb.consents.confirmation.enabled", "false")
-    }
-
-    signPlugin {
-        certificateChain.set(environment("CERTIFICATE_CHAIN"))
-        privateKey.set(environment("PRIVATE_KEY"))
-        password.set(environment("PRIVATE_KEY_PASSWORD"))
-    }
-    
-// ########## DEACTIVATED too much effort to debug changes every new version ##########
-//    publishPlugin {
-//        dependsOn("patchChangelog")
-//        token.set(environment("PUBLISH_TOKEN"))
-//        // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
-//        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-//        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-//        channels.set(properties("pluginVersion").map {
-//            listOf(
-//                it.split('-').getOrElse(1) { "default" }.split('.').first()
-//            )
-//        })
-//    }
-
-    withType<Test> {
-        systemProperty("idea.test.execution.policy", "com.github.yunabraska.githubworkflow.services.PluginExecutionPolicy")
-        systemProperty("PLUGIN_HOME_PATH", rootProject.file("src/test/resources"))
-    }
+    repositoryUrl.set(pluginUrl)
 }
